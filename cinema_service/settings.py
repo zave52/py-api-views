bd7fb8a7cd5e189c069965ea074985ffa969c6f3
zakhar <zaharsavchen@gmail.com>
2025-01-22 20:28:34 +0200

    add second argument to get secret key

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-ru6ndfka@95_(lysua8yhdjq@vpiqgv3yru4r)q3h4_u8x7dfy")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
